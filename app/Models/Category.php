0dda8038d66505a96c90d8d051b7a6d5e940a2cb
Reajul Islam Mojumder Robin <reajulbd786@gmail.com>
2021-11-03 23:49:03 +0600

    DB changes

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -19,13 +19,9 @@ class Category extends Model
     }
     public function delete()
     {
-        // delete all related photos
-        $this->product()->delete();
-        // as suggested by Dirk in comment,
-        // it's an uglier alternative, but faster
-        // Photo::where("user_id", $this->id)->delete()
 
-        // delete the user
+        $this->product()->delete();
+
         return parent::delete();
     }
 }
