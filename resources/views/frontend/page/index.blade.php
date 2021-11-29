fc2a003a972b5c572e7b49a66dc16ca6e3a15ed7
Reajul Islam Mojumder Robin <reajulbd786@gmail.com>
2021-11-29 20:50:46 +0600

    phone view

--- resources/views/frontend/page/index.blade.php
+++ resources/views/frontend/page/index.blade.php
@@ -99,30 +99,7 @@
           </a>
         </div>
       </div>
-        <div class="col-md-6 col-lg-4 p-b-30 m-lr-auto">
-            <!-- Block1 -->
-            <div class="block1 wrap-pic-w">
-                <img src="images/falgun.jpg" alt="IMG-BANNER">
 
-                <a href="{{ route('product.tag',['tag'=>'Falgun Lookbook']) }}" class="block1-txt ab-t-l s-full flex-col-l-sb p-lr-38 p-tb-34 trans-03 respon3">
-                    <div class="block1-txt-child1 flex-col-l">
-              <span class="block1-name ltext-102 trans-04 p-b-8">
-               Falgun Lookbook
-              </span>
-
-                        <span class="block1-info stext-102 trans-04">
-                Spring 2021
-              </span>
-                    </div>
-
-                    <div class="block1-txt-child2 p-b-4 trans-05">
-                        <div class="block1-link stext-101 cl0 trans-09">
-                            Shop Now
-                        </div>
-                    </div>
-                </a>
-            </div>
-        </div>
 
       <div class="col-md-6 col-lg-4 p-b-30 m-lr-auto">
         <!-- Block1 -->
